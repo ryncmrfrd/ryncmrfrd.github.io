0bf4bc24b56386b03a6a2160b6952d81b498ce6d
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2018-09-06 20:13:27 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,13 @@
-var isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
-
-var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-
-function changeCSSBasedOnDevice() {
-
-    if (w > h){
-
-        document.getElementById("button1").className = "buttonHorizontal";
-        document.getElementById("button2").className = "buttonHorizontal";
-        document.getElementById("button3").className = "buttonHorizontal";
-        document.getElementById("button4").className = "buttonHorizontal";
-        document.getElementById("button5").className = "buttonHorizontal";
-        document.getElementById("card").className = "cardhorizontal";    
-    }
-    else{
-
-        document.getElementById("button1").className = "buttonVertical";
-        document.getElementById("button2").className = "buttonVertical";
-        document.getElementById("button3").className = "buttonVertical";
-        document.getElementById("button4").className = "buttonVertical";
-        document.getElementById("button5").className = "buttonVertical";
-        document.getElementById("card").className = "cardvertical";
-    
-    }
-
-}
+var swiper = new Swiper('.blog-slider', {
+      spaceBetween: 30,
+      effect: 'fade',
+      loop: true,
+      mousewheel: {
+        invert: false,
+      },
+      // autoHeight: true,
+      pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+      }
+    });
