df3e8efe593b261fa01715cb4661909a26e317de
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2018-10-26 13:27:40 +1100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-document.onkeydown = function(e){
-    if (e.keyCode == 27) {
-      var mods = document.querySelectorAll('.modal > [type=checkbox]');
-      [].forEach.call(mods, function(mod){ mod.checked = false; });
-    }
-  }
