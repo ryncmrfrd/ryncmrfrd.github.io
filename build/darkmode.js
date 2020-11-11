67e87927f10d00a1bdd4f6555d9ed54570868a7b
Ryan Comerford <ryncmrfrd@outlook.com>
2020-11-11 11:47:17 +1100

    added auto-redirect to 404 page

--- build/darkmode.js
+++ build/darkmode.js
@@ -1,15 +1,17 @@
+const body = document.documentElement
+
 //auto set based on time
 const hour = new Date().getHours(); 
 (hour > 18 || hour < 7) ? 
-    document.documentElement.setAttribute('data-theme','dark') : 
-    document.documentElement.setAttribute('data-theme','light');
+    body.setAttribute('data-theme','dark') : 
+    body.setAttribute('data-theme','light');
 
 //set switch based on data-theme
-document.getElementById("switch").checked = (document.documentElement.getAttribute("data-theme") == "dark");
+document.getElementById("switch").checked = (body.getAttribute("data-theme") == "dark");
 
 //handle switch changes
 document.getElementById("switch").addEventListener("change", e => {
-    (e.target.checked) ? 
-        document.documentElement.setAttribute('data-theme','dark') : 
-        document.documentElement.setAttribute('data-theme','light') 
+    e.target.checked ? 
+        body.setAttribute('data-theme','dark') : 
+        body.setAttribute('data-theme','light') 
 });
---
+++ build/redirect.js
@@ -0,0 +1,9 @@
+const changeRedirectNum = (elem) => {
+    var currentNum = Number(elem.innerText)
+    currentNum-1 == 0 ?
+        window.location.replace('/') :
+        (elem.innerText = currentNum-1, setTimeout(() => changeRedirectNum(elem), 1000))
+
+}
+
+changeRedirectNum(document.getElementById("redirectNum"))
