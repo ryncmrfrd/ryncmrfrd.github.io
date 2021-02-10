bbeb4a55f0dbbaad3f0ca5b77b5c17b241539ca8
Ryan Comerford <ryncmrfrd@outlook.com>
2021-02-10 12:18:58 +1100

    Add location-based day-night cycle

--- build/darkmode.js
+++ build/darkmode.js
@@ -1,17 +1,34 @@
-const body = document.documentElement
-
-//auto set based on time
-const hour = new Date().getHours(); 
-(hour > 18 || hour < 7) ? 
-    (body.setAttribute('data-theme','dark'), document.getElementById("switch") = checked) : 
-    body.setAttribute('data-theme','light');
-
-//set switch based on data-theme
-document.getElementById("switch").checked = (body.getAttribute("data-theme") == "dark");
+var body = document.documentElement,
+    chkbox = document.getElementById("switch"),
+    hour = new Date().getHours();
 
 //handle switch changes
-document.getElementById("switch").addEventListener("change", e => {
+chkbox.addEventListener("change", e => {
     e.target.checked ? 
         body.setAttribute('data-theme','dark') : 
         body.setAttribute('data-theme','light');
 });
+
+//set switch based on data-theme
+body.getAttribute("data-theme") == "dark" ? chkbox.click() : "";
+
+//auto set based on time
+if(!navigator.geolocation) location_fb();
+else {
+    navigator.geolocation.getCurrentPosition(d => {
+        fetch(`https://api.sunrise-sunset.org/json?lat=${d.coords.latitude}&lng=${d.coords.longitude}&date=today&formatted=0`)
+        .then(r => r.json())
+        .then(d => { 
+            let hour_srse = new Date(Date.parse(d.results.sunrise)).getHours(),
+                hour_sset = new Date(Date.parse(d.results.sunset)).getHours();
+            (hour > hour_sset || hour < hour_srse) && (!chkbox.checked) ? chkbox.click() : "";
+        })
+        .catch( e => location_fb(e));
+    }, e => location_fb(e));
+}
+
+//fallback fn for auto set dark mode
+const location_fb = (e) => {
+    (hour > 18 || hour < 7) && (!chkbox.checked) ? chkbox.click() : "";
+    console.warn("Error fetching sunrise/sunset times. Details: ", e)
+}
