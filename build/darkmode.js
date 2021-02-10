444b6c9f58175219dc8f178902cbd547997ef29e
Ryan Comerford <ryncmrfrd@outlook.com>
2021-02-11 09:57:04 +1100

    Run darkmode code without prompt if permanant permission given

--- build/darkmode.js
+++ build/darkmode.js
@@ -12,25 +12,30 @@ chkbox.addEventListener("change", e => {
 //set switch based on data-theme
 body.getAttribute("data-theme") == "dark" ? chkbox.click() : "";
 
-document.querySelector("#prompt").addEventListener("click", d => {
-    //auto set based on time
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(d => {
-			document.querySelector(".swichPrompt").classList.add("clicked");
-            fetch(`https://api.sunrise-sunset.org/json?lat=${d.coords.latitude}&lng=${d.coords.longitude}&date=today&formatted=0`)
-            .then(r => r.json())
-            .then(d => { 
-                let hour_srse = new Date(Date.parse(d.results.sunrise)).getHours(),
-                    hour_sset = new Date(Date.parse(d.results.sunset)).getHours();
-                (hour > hour_sset || hour < hour_srse) && (!chkbox.checked) ? chkbox.click() : "";
-            })
-            .catch( e => location_fb(e));
-        }, e => location_fb(e));
-	}
-});
-
-//fallback fn for auto set dark mode
-const location_fb = (e) => {
-	document.querySelector(".swichPrompt").classList.add("clicked");
-    console.warn("Error fetching sunrise/sunset times. Details: ", e)
+if(!navigator.geolocation) locationFallback("Geolocation API not supported by browser.");
+else{
+    navigator.permissions.query({ name: 'geolocation' }).then(function(result) {
+        if(result.state == 'granted') {
+            getLocation();
+        } else{
+            document.querySelector("#prompt").addEventListener("click", getLocation)
+        }
+    });
 }
+
+//get location and run autodark mode fn
+const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(d => {
+        fetch(`https://api.sunrise-sunset.org/json?lat=${d.coords.latitude}&lng=${d.coords.longitude}&date=today&formatted=0`)
+        .then(r => r.json())
+        .then(d => { 
+            let hour_srse = new Date(Date.parse(d.results.sunrise)).getHours(),
+                hour_sset = new Date(Date.parse(d.results.sunset)).getHours();
+            (hour > hour_sset || hour < hour_srse) && (!chkbox.checked) ? chkbox.click() : "";
+            document.querySelector(".swichPrompt").classList.add("clicked");
+        })
+        .catch( e => locationFallback(e));
+    }, e => locationFallback(e));
+}
+
+const locationFallback = (e) => console.warn("Error fetching sunrise/sunset times. Details: ", e);
